e38655c104ca8ba35c260ad973bbba5741aa890f
vikash kumar <kumarvikash6101998@gmail.com>
2021-09-11 07:44:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const text = "welcome to my own created website";
+let index =0;
+function writeText(){
+    document.body.innerText = text.slice(0,index);
+    index++
+    if(index>text.length){
+        index=0;
+    }
+}
+setInterval(writeText, 100);
